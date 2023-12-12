d3b1f65eea84ac165731fbeaa5daf1b40d3c206e
Henrik Simons <37830761+henriksimons@users.noreply.github.com>
2023-12-12 10:07:36 +0100

    Update InputForm.js

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,114 +1,120 @@
 import { useState } from "react";
 
 const InputForm = (props) => {
+  const [id, setId] = useState(0);
+  const [repayment, setRepayment] = useState(0.5);
+  const [cost, setCost] = useState(0);
+  const [name, setName] = useState("");
   const [person, setPerson] = useState("");
-  const [cost, setCost] = useState("");
-  const [expense, setExpense] = useState("");
-  const [compensation, setCompensation] = useState("0.5");
 
   const submitForm = (event) => {
     event.preventDefault();
 
     const item = {
-      expense: expense,
+      id: id,
+      name: name,
       cost: cost,
       person: person,
-      compensation: compensation,
+      repayment: repayment,
     };
+
+    setId(id + 1);
 
     props.onExpenseSubmit(item);
   };
 
-  const handleCost = (event) => {
-    setCost(event.target.value);
+  const handleCost = (e) => {
+    setCost(e.target.value);
   };
 
-  const handleExpense = (event) => {
-    setExpense(event.target.value);
+  const handleName = (e) => {
+    setName(e.target.value);
   };
 
-  const handleCompensation = (event) => {
-    setCompensation(event.target.value);
+  const handleRepayment = (e) => {
+    setRepayment(e.target.value);
   };
 
-  const handlePerson = (event) => {
-    setPerson(event.target.value);
+  const handlePerson = (e) => {
+    setPerson(e.target.value);
   };
 
   return (
     <>
-      <div>
-        <h1>Kostnadsformulär</h1>
-        <form onSubmit={submitForm}>
-          <div className="form-group mt-2">
-            <label htmlFor="expense">Utgiftspost</label>
-            <input
-              className="form-control"
-              id="expense"
-              name="expense"
-              onChange={handleExpense}
-              placeholder="Ange utgiftspost"
-              type="text"
-            ></input>
-          </div>
-          <div className="form-group mt-2">
-            <label htmlFor="cost">Kostnad</label>
-            <input
-              className="form-control"
-              id="cost"
-              name="cost"
-              onChange={handleCost}
-              placeholder="Ange kostnad"
-              type="text"
-            ></input>
-          </div>
-          <div className="form-group mt-2">
-            <label htmlFor="compensation">Ange ersättningsgrad</label>
-            <select
-              className="form-control"
-              id="compensation"
-              name="compensation"
-              onChange={handleCompensation}
-              value={compensation}
-            >
-              <option value="0.5">50%</option>
-              <option value="1">100%</option>
-            </select>
-          </div>
+      <div className="row">
+        <div className="col-5">
+          <h1>Kostnadsformulär</h1>
+          <form onSubmit={submitForm}>
+            <div className="form-group mt-2">
+              <label htmlFor="expense">Utgiftspost</label>
+              <input
+                className="form-control"
+                id="expense"
+                name="expense"
+                onChange={handleName}
+                placeholder="Ange utgiftspost"
+                type="text"
+              ></input>
+            </div>
+            <div className="form-group mt-2">
+              <label htmlFor="cost">Kostnad</label>
+              <input
+                className="form-control"
+                id="cost"
+                name="cost"
+                onChange={handleCost}
+                placeholder="Ange kostnad"
+                type="text"
+              ></input>
+            </div>
+            <div className="form-group mt-2">
+              <label htmlFor="repayment">Ange ersättningsgrad</label>
+              <select
+                className="form-control"
+                id="repayment"
+                name="repayment"
+                onChange={handleRepayment}
+                value={repayment}
+              >
+                <option value="0.5">50%</option>
+                <option value="1">100%</option>
+              </select>
+            </div>
 
-          <div className="form-group">
-            <label className="col-12 mb-1 mt-2">Utläggare</label>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                id="henrik"
-                name="flexRadioDefault"
-                onClick={handlePerson}
-                type="radio"
-                value="Henrik"
-              ></input>
-              <label className="form-check-label" htmlFor="henrik">
-                Henrik
-              </label>
+            <div className="form-group">
+              <label className="col-12 mb-1 mt-2">Utläggare</label>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  id="henrik"
+                  name="flexRadioDefault"
+                  onClick={handlePerson}
+                  type="radio"
+                  value="Henrik"
+                ></input>
+                <label className="form-check-label" htmlFor="henrik">
+                  Henrik
+                </label>
+              </div>
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  id="ida"
+                  name="flexRadioDefault"
+                  onClick={handlePerson}
+                  type="radio"
+                  value="Ida"
+                ></input>
+                <label className="form-check-label" htmlFor="ida">
+                  Ida
+                </label>
+              </div>
             </div>
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                id="ida"
-                name="flexRadioDefault"
-                onClick={handlePerson}
-                type="radio"
-                value="Ida"
-              ></input>
-              <label className="form-check-label" htmlFor="ida">
-                Ida
-              </label>
-            </div>
-          </div>
-          <button type="submit" className="btn btn-primary mt-2">
-            Submit
-          </button>
-        </form>
+            <button type="submit" className="btn btn-success mt-2">
+              Lägg till
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
