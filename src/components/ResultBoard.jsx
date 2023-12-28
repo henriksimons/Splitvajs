29526629287ef1e1efe7c5f4e0516113b751e3f1
henriksimons <henriksimons2@gmail.com>
2023-12-28 23:27:24 +0100

    Add conditional result

--- src/components/ResultBoard.jsx
+++ src/components/ResultBoard.jsx
@@ -20,36 +20,44 @@ const ResultBoard = ({ onRefresh }) => {
       <div className="row">
         <h2>Resultat</h2>
       </div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">
-              <h5>Namn</h5>
-            </th>
-            <th scope="col">
-              <h5>Utgifter</h5>
-            </th>
-            <th scope="col">
-              <h5>Skuld</h5>
-            </th>
-            <th scope="col">
-              <h5>Resultat</h5>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {result.map((result) => {
-            return (
-              <tr key={result.personId}>
-                <td>{result.personName}</td>
-                <td>{result.totalExpenses} kr</td>
-                <td>{result.totalDebt} kr</td>
-                <td>{result.balance} kr</td>
+      {result.length !== 0 ? (
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">
+                  <h5>Namn</h5>
+                </th>
+                <th scope="col">
+                  <h5>Utgifter</h5>
+                </th>
+                <th scope="col">
+                  <h5>Skuld</h5>
+                </th>
+                <th scope="col">
+                  <h5>Resultat</h5>
+                </th>
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+              {result.map((result) => {
+                return (
+                  <tr key={result.personId}>
+                    <td>{result.personName}</td>
+                    <td>{result.totalExpenses} kr</td>
+                    <td>{result.totalDebt} kr</td>
+                    <td>{result.balance} kr</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <div>
+          <p>Inget resultat ännu...</p>
+        </div>
+      )}
     </div>
   );
 };
